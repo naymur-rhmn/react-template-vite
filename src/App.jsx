0eeebf8ca97a2ab39316b81b28c8bcb837dfc582
naymur <naymurmn@gmail.com>
2023-11-27 10:54:02 +0600

    template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 style={{ textAlign: 'center', marginTop: '250px' }}>
+        React Starter Template
+      </h1>
+    </>
+  );
+}
+
+export default App;
